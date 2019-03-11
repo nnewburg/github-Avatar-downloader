63e93d38a4aedcdf13483daa412c7b786164737a
Nick <pickme1152019@gmail.com>
2019-03-11 23:49:16 +0000

    added a few more comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,21 +1,17 @@
 var request = require('request');
 var secrets = require('./secrets');
 var fs = require('fs');
-
 console.log('Welcome to the GitHub Avatar Downloader!');
-
 
 //main function to input name of the repository to and a callback function to execute a muttable
 //function
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
   //conditional to terminate program if an argument is not passed
   if(arguments[0] === undefined || arguments[1] === undefined){
-    return console.log("Error input a name or repo!!!!!!!!!!!!!!!!!!!!")
-
+    return console.log("Error input a name or repo!!!!!!!!!!!!!!!!!!!!");
   }
 
-  //object to authorize the request function and its target
+  //object to authorize the request function and its target(repoOwner/repoName)
  var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -24,22 +20,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-
     //request function to parse the string of objects
     //iterate through the object and apply the call back function onto each of the
     //objects found
-    request(options, function(err, res, body) {
-      let output = JSON.parse(body)
+  request(options, function(err, res, body) {
+    let output = JSON.parse(body);
 
-      for(key in output) {
-       // console.log(output[key]);
-
-        cb(output[key].avatar_url, 'avatars/' + output[key].login+'.jpg')
-      }
-    });
-
-
-
+    for(key in output) {
+      cb(output[key].avatar_url, 'avatars/' + output[key].login+'.jpg');
+    }
+  });
 }
 
 //creates two variables to intake command line arguments
@@ -49,11 +39,8 @@ let name = process.argv[3];
 //call the function with the input values and callback function
 getRepoContributors(owner, name, downloadImageByURL)
 
-
-
 //the call back function that gets the image URL and writes the image to a new folder
 function downloadImageByURL(url, filePath) {
-
   request.get(url)
        .on('error', function (err) {
          throw err;
